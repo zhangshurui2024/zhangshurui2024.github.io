0ed532e0a8cbfbf6dc58625bafad75a51df546f3
zhangshurui2024 <2586313913@qq.com>
2024-08-08 19:05:30 +0800

    Site updated: 2024-08-08 19:05:23

--- js/fomal.js
+++ js/fomal.js
@@ -73,7 +73,7 @@ $.ajax({
   type: 'get',
   url: 'https://apis.map.qq.com/ws/location/v1/ip',
   data: {
-    key: '',  // 这里要写你的KEY!!!
+    key: '2ZEBZ-3W3WN-VEHFK-SUTNR-DCXHH-STBNF',  // 这里要写你的KEY!!!
     output: 'jsonp',
   },
   dataType: 'jsonp',
